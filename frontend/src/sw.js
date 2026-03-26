dc056749a6b3f728d6d9cd08d714c89f58eb4353
Raquib Reyaz <raquibreyaz111@gmail.com>
2026-03-26 16:52:14 +0530

    feat: remove PWA plugin, service worker implementation, and registration.

--- frontend/src/sw.js
+++
@@ -1,9 +0,0 @@
-import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
-
-self.__WB_MANIFEST;
-
-cleanupOutdatedCaches();
-
-precacheAndRoute(self.__WB_MANIFEST);
-
-self.skipWaiting();
