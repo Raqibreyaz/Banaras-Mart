0abcf342fd80d7fad6a327bb9c1b52a19ad365ed
Raquib Reyaz <raquibreyaz111@gmail.com>
2026-03-26 14:38:49 +0530

    - fixed email sending problem by migrating to nodemailer - placed pnpm store to /var/tmp due to symlinking issues - added vercel.json to support direct page viewing

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,29 +1,38 @@
-import sgMail from '@sendgrid/mail';
-import jwt from 'jsonwebtoken'
-import { ApiError } from './ApiError.js';
+import nodemailer from "nodemailer";
+import jwt from "jsonwebtoken";
+import { ApiError } from "./ApiError.js";
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: 587,
+  secure: false, // Use true for port 465, false for port 587
+  auth: {
+    user: process.env.NODEMAILER_EMAIL,
+    pass: process.env.NODEMAILER_PASS,
+  },
+});
 
 export const sendEmail = async (user) => {
+  const { email, _id, fullname } = user;
 
-    const { email, _id, fullname } = user
+  const { FRONTEND_URL, JWT_SECRET_KEY } = process.env;
 
-    const { FRONTEND_URL, JWT_SECRET_KEY } = process.env
+  if (!FRONTEND_URL || !JWT_SECRET_KEY)
+    throw new ApiError(400, "missing environment variables");
 
-    if (!FRONTEND_URL || !JWT_SECRET_KEY)
-        throw new ApiError(400, "missing environment variables")
+  const token = jwt.sign({ email, userId: _id }, JWT_SECRET_KEY, {
+    expiresIn: 1800,
+  });
 
-    const token = jwt.sign({ email, userId: _id }, JWT_SECRET_KEY, { expiresIn: 1800 })
+  const resetLink = `${FRONTEND_URL}/reset-password?token=${token}`;
 
-    const resetLink = `${FRONTEND_URL}/reset-password?token=${token}`;
+  const msg = {
+    to: email,
+    from: `Banaras Mart ${process.env.NODEMAILER_EMAIL}`,
+    subject: "Password Reset Request",
+    text: `Hi ${fullname} you or someone on your behalf requested a password reset. Click the link below to reset your password:\n\n${resetLink}`,
+    html: `<p>Hi ${fullname} you or someone on your behalf requested a password reset. Click the link below to reset your password:</p><a href="${resetLink}">Reset Password</a>`,
+  };
 
-    const msg = {
-        to: email,
-        from: 'BanarasMart123@gmail.com', // Use your verified sender
-        subject: 'Password Reset Request',
-        text: `Hi ${fullname} you or someone on your behalf requested a password reset. Click the link below to reset your password:\n\n${resetLink}`,
-        html: `<p>Hi ${fullname} you or someone on your behalf requested a password reset. Click the link below to reset your password:</p><a href="${resetLink}">Reset Password</a>`,
-    };
-
-    return await sgMail.send(msg)
+  return await transporter.sendMail(msg);
 };
